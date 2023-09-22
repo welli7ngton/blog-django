faa6ec21c50b97160885a3091e9e46897596f2ed
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-22 14:14:17 -0300

    ajustando área administrativa dos models

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
+from blog.models import Tags, Category, Page, Post
 
-# Register your models here.
+
+@admin.register(Tags)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'slug',
+    )
+    list_display_links = (
+        'name',
+    )
+    search_fields = (
+        'id', 'name', 'slug',
+    )
+    list_per_page = 10
+
+    ordering = ('-id',)
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'slug',
+    )
+    list_display_links = (
+        'name',
+    )
+    search_fields = (
+        'id', 'name', 'slug',
+    )
+    list_per_page = 10
+
+    ordering = ('-id',)
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'slug', 'is_published',
+    )
+    list_display_links = (
+        'title',
+    )
+    search_fields = (
+        'id', 'title', 'slug', 'is_published',
+    )
+    list_per_page = 10
+    list_filter = ('is_published',)
+    list_editable = ('is_published',)
+    ordering = ('-id',)
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # summernote_fiedls = 'content'
+    list_display = (
+        'id', 'title', 'is_published', 'created_by'
+    )
+    list_display_links = (
+        'title',
+    )
+    search_fields = (
+        'id', 'slug', 'title', 'excerpt', 'content',
+    )
+    list_per_page = (50)
+    list_filter = (
+        'category', 'is_published',
+    )
+    list_editable = ('is_published',)
+    ordering = ('-id',)
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by',
+    )
+    prepopulated_fields = {
+        'slug': ('title', ),
+    }
+    autocomplete_fields = (
+        'tags', 'category',
+    )
