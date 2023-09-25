a44d339e8bba274bfd2c7f84425dbbb9291475bc
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-25 14:47:15 -0300

    adicionando path do summernote nas urls do projeto

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls')),
 ]
 
