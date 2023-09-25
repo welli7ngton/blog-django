a3ab16f11d51c331c34e997b96af95ee414ee8cb
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-25 14:45:43 -0300

    criando post attachment

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
+from utils.images import resize_image
+from django_summernote.models import AbstractAttachment
+
+
+class PostAttachment(AbstractAttachment):
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name == self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900, optimize=True, quality=70)
+
+        return super_save
 
 
 class Tags(models.Model):
@@ -104,4 +124,15 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title)
-        return super().save(*args, **kwargs)
+
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, optimize=True, quality=70)
+
+        return super_save
