269d00ba09ae2b911e269cea09418568a1a35922
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 12:36:51 -0300

    usando a função get_absolute_url para gerar as urls dos posts

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -3,6 +3,7 @@ from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.images import resize_image
 from django_summernote.models import AbstractAttachment
+from django.urls import reverse
 
 
 class PostManager(models.Manager):
@@ -128,6 +129,12 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+
+        return reverse('blog:post', args=(self.slug,))
 
     def save(self, *args, **kwargs):
         if not self.slug:
