3bf881258f60afbd9a9a00e8d7910204fee8ccee
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 14:41:21 -0300

    criando urls para created_by e caregory

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,5 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<slug:slug>/', views.post, name='post'),
-    path('page/', views.page, name='page'),
+    path('post/<slug:slug>/', views.page, name='page'),
+    path('created_by/<int:_id>/', views.created_by, name='created_by'),
+    path('category/<slug:slug>/', views.category, name='category'),
 ]
