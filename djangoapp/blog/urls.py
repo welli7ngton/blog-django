48b401d083a25d22eeb12733d96069521e2300c3
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 10:41:16 -0300

    adicionando url para o post com a slug do próprio post

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/', views.post, name='post'),
+    path('post/<slug:slug>/', views.post, name='post'),
     path('page/', views.page, name='page'),
 ]
