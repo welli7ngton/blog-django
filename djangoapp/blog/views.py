43512d37ea8aa42429766cca9d3288c4fca61673
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 11:25:24 -0300

    usando o método criado dentro do meu manager

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,7 +6,7 @@ PER_PAGE = 9
 
 
 def index(request):
-    posts = Post.objects.filter(is_published=True).order_by('-pk')
+    posts = Post.objects.get_published()
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
