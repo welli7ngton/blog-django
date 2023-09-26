5e87433357227c190dddd5c977ffb3e93b8195a7
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 10:42:16 -0300

    começando a usar os posts reais do projeto

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,13 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from blog.models import Post
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 
 def index(request):
-    paginator = Paginator(posts, 9)
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -19,28 +21,21 @@ def index(request):
 
 
 def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
     return render(
         request,
         'blog/pages/page.html',
         {
-            'page_obj': page_obj,
+            # 'page_obj': page_obj,
         }
     )
 
 
 def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            'page_obj': page_obj,
+            # 'page_obj': page_obj,
         }
     )
