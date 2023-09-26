213e6017ddf0cfa3a992191b7b8635a6c6baa4d9
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-26 14:40:46 -0300

    criando views pra created_by e post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -20,7 +20,39 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, _id):
+    posts = Post.objects.get_published().filter(created_by__pk=_id)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -30,12 +62,14 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+
+    single_post = Post.objects.get_published().filter(slug=slug).first()
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': single_post,
         }
     )
