dfca2aaa39e7af670008bc1a45f029fb8e9dc9e5
welli7ngton <wellingtonasilva45@gmail.com>
2023-09-27 12:22:00 -0300

    deixando a 'search' do site funcional

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('created_by/<int:_id>/', views.created_by, name='created_by'),
     path('category/<slug:slug>/', views.category, name='category'),
     path('tag/<slug:slug>/', views.tag, name='tag'),
+    path('searcgh/', views.search, name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -64,6 +65,24 @@ def tag(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+
+def search(request):
+    search_value = request.GET.get('search').strip()
+    posts = Post.objects.get_published().filter(
+        Q(title__icontains=search_value) |
+        Q(exerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+    )[0:PER_PAGE]
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value
         }
     )
 
